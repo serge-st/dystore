958262b299abd9d83ce6c689490a0392ec777405
Sergejs Stecenko <sergejs.stecenko@gmail.com>
2023-04-21 13:55:07 +0300

    add picture func

--- src/components/GridItem/GridItem.js
+++ src/components/GridItem/GridItem.js
@@ -1,6 +1,7 @@
 const GridItem = ({data}) => {
     return (
         <div>
+            <img src={`https://picsum.photos/id/${data.id}/200/300`} alt={data.title}/>
             Grid item {data.id}
         </div>
     );
